4432269cdcf603f29503bd3593e4da2e2b19ba1c
Thanh Dat <tthanhdat1302@gmail.com>
2021-03-31 20:28:23 +0700

    Create DataContext

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
